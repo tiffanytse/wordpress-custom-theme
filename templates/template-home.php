c52ba3d393ff9add1c7465218db0b30e25ef8bec
Tiffany Tse <ttse@coldfrontlabs.ca>
2015-10-05 00:53:01 -0400

    feat(*): add initial files

---
+++ templates/template-home.php
@@ -0,0 +1,49 @@
+<?php 
+/*
+Template Name: Home Page Template (or whatever you want to call it)
+*/
+?>
+<?php
+  get_header();
+  the_post();
+?>
+
+<!-- Slider -->
+
+<h1><?php the_title(); ?></h1>
+<?php the_content(); ?>
+
+<ol>
+  <?php 
+    $posts = get_posts([
+      'numberposts' => 3, 
+      'category_name' => 'featured'
+    ]);
+
+    foreach ($posts as $post) :
+      setup_postdata($post);
+  ?>
+
+  <li>
+    <h2><?php the_title(); ?></h2>
+    <span><?php the_excerpt(); ?></span>
+  </li>
+
+  <?php endforeach; ?>
+</ol>
+
+<?php
+  get_footer();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
